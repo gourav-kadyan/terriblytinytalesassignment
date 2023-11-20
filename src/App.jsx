c1ac46c8055840e6d572a77cb636161b1aae9e4b
gourav-kadyan <gouravkadyan555@gmail.com>
2023-11-20 21:36:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='container'>
+          <div className='top'>
+            <h1>top</h1>
+          </div>
+          <div className='bottom'>
+            <h1>bottom</h1>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
